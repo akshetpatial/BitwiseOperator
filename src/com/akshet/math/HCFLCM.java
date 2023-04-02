d862415ce96105aef19a3c1b8673fa0afd1a8d0f
AKSHET PATIAL <akshet.patial@gmail.com>
2023-04-02 14:13:56 +0530

    Added the LCM and HCF method

--- src/com/akshet/math/HCFLCM.java
+++ src/com/akshet/math/HCFLCM.java
@@ -4,7 +4,22 @@ import java.util.Scanner;
 
 public class HCFLCM {
 
-    
+    // highest common factor we can use this to solve the linear equations
+    // We are finding the HCF using Recursion
+    public static int HCF(int num_1, int num_2){
+        if(num_1==0)
+            return num_2;
+        return HCF(num_2%num_1,num_1);
+    }
+
+    // Least common multiple
+    // We know that num_1 * num_2 = LCM * HCF
+    // Product of the two numbers is equal to product pf their LCM and HCF
+    public static int LCM(int num_1, int num_2){
+        int lcm = (num_1 * num_2)/HCF(num_1,num_2);
+        return lcm;
+    }
+
     public static void main(String[] args) {
         System.out.println("Enter the first number: ");
         Scanner scan = new Scanner(System.in);
